1acf8dd360343978d3e307a01155e114100006a1
Immutare <carlos.gg.eduardo@gmail.com>
2018-01-14 23:36:50 -0600

    lazy fix

--- eu4.py
+++ eu4.py
@@ -4,14 +4,25 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
-num1 = 999
-num2 = 999
 
 def isPalin(num):
     newNum = str(num)
     cant = 0
     if (len(newNum) % 2 == 0):
         cant = len(newNum) / 2
-        if newNum[::cant] == newNum[::-cant]:
+        reversedNum = newNum[::-1]
+        if newNum[:cant] == reversedNum[:cant]:
+            # print newNum[:cant]
+            # print reversedNum[:cant]
             return True
         return False
+
+listNum = []
+
+for i in range(999, 450, -1):
+    for j in range(999, 450, -1):
+        if isPalin(i*j):
+            listNum.append(i*j)
+
+listNum.sort(reverse = True)
+print listNum[0]
